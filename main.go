0b8b849b837bbbc296cb2825ee250253a96967eb
Cedric Ahlers <55838528+clowa@users.noreply.github.com>
2024-12-19 22:15:07 +0000

    increase readability by using object oriented programming for configuration

--- main.go
+++ main.go
@@ -24,15 +24,12 @@ var (
 	regionFlag = flag.String("region", "us-west-2", "The AWS region to use. Example: us-west-2")
 )
 
-type configuration struct {
-	watch_dir     string
-	bucket_name   string
-	bucket_prefix string
-	aws_region    string
-}
-
 func main() {
-	if !loadConfig() {
+	// Initialize the application
+	inizialize()
+
+	// Load configuration values
+	if success := config.Load(); !success {
 		log.Fatal("Failed to load configuration")
 	}
 	log.Print("Starting S3 File Watcher")
@@ -66,40 +63,52 @@ func main() {
 	wg.Wait()
 }
 
-// loadConfig loads configuration values from environment variables and run validation checks.
+func inizialize() {
+	// Log to stdout
+	log.SetOutput(os.Stdout)
+}
+
+type configuration struct {
+	watch_dir     string
+	bucket_name   string
+	bucket_prefix string
+	aws_region    string
+}
+
+// Load loads configuration values from environment variables and run validation checks.
 // It returns true if the configuration values are valid, false otherwise.
-func loadConfig() bool {
+func (c *configuration) Load() bool {
 	// Parse CLI flags
 	flag.Parse()
 
-	config.aws_region = *regionFlag
+	c.aws_region = *regionFlag
 
 	// Load configuration from CLI flags or environment variables
 	if *sourceFlag != "" {
-		config.watch_dir = *sourceFlag
+		c.watch_dir = *sourceFlag
 	} else {
-		config.watch_dir = os.Getenv("WATCH_DIR")
+		c.watch_dir = os.Getenv("WATCH_DIR")
 	}
 
 	if *bucketFlag != "" {
-		config.bucket_name = *bucketFlag
+		c.bucket_name = *bucketFlag
 	} else {
-		config.bucket_name = os.Getenv("S3_BUCKET_NAME")
+		c.bucket_name = os.Getenv("S3_BUCKET_NAME")
 	}
 
 	if *prefixFlag != "" {
-		config.bucket_prefix = *prefixFlag
+		c.bucket_prefix = *prefixFlag
 	} else {
-		config.bucket_prefix = os.Getenv("S3_BUCKET_PREFIX")
+		c.bucket_prefix = os.Getenv("S3_BUCKET_PREFIX")
 	}
 
 	// Validate configuration values
-	return validateConfig()
+	return c.Validate()
 }
 
-// validateConfig encapsulates the validation logic for the configuration values.
+// Validate encapsulates the validation logic for the configuration values.
 // It returns true if the configuration values are valid, false otherwise.
-func validateConfig() bool {
+func (c *configuration) Validate() bool {
 	// Validate source directory
 	if _, err := os.Stat(config.watch_dir); os.IsNotExist(err) {
 		log.Printf("Invalid source directory. Please provide a valid directory path. Example: /path/to/source")
